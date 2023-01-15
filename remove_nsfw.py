f2b89d6713d83758bb1f63a0e7d95b2c4a332de9
LucaCorvitto <87773518+LucaCorvitto@users.noreply.github.com>
2023-01-15 21:11:14 +0100

    Update remove_nsfw.py

--- remove_nsfw.py
+++ remove_nsfw.py
@@ -2,6 +2,13 @@ import os
 from PIL import Image
 import shutil
 
+def create_dir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+        print(f'created folder: {path}')
+    else:
+        print(f'directory {path} already exists')
+
 def main():
     for path in ['datasets/eval/fake','datasets/test/fake','datasets/train/fake']:
         images = os.listdir(path)
@@ -15,6 +22,8 @@ def main():
                 #print(image_path)
                 # delete the image from the folder
                 os.remove(os.path.join(path, image_path))
+                # If one wants to keep the nsfw images for ulterior analysis, one can move them to another folder using the two following rows of code. Remember to comment the precedent row
+                #create_dir('datasets/nsfw')
                 #shutil.move(os.path.join(path, image_path), os.path.join('datasets/nsfw', image_path))
         print(f'deleted images from {path}: {del_imgs}')
 
